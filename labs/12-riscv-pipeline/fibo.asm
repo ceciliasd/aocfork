331b47e4ec1746ef2ea1b20503a0a1b3dfe0c4bd
ceciliasd <ceciliasd@estudante.ufscar.br>
2025-12-15 16:48:24 -0300

    Refactor Fibonacci

--- labs/12-riscv-pipeline/fibo.asm
+++ labs/12-riscv-pipeline/fibo.asm
@@ -1,21 +1,22 @@
 .text
 .globl _start
 _start:
-    la   s0, fib_data      # ponteiro para RAM (dados)
-    la   s1, fib_out       # endereço de saída (ex: memória mapeada ou RAM)
+    la   s0, fib_data        # ponteiro para RAM
+    li   s1, 0x00000104      # endereço MMIO dos LEDs
 
-    lw   t0, 0(s0)         # t0 = fib[0]
-    lw   t1, 4(s0)         # t1 = fib[1]
+    lw   t0, 0(s0)           # fib(0)
+    lw   t1, 4(s0)           # fib(1)
 
 loop:
-    add  t2, t0, t1        # t2 = fib(n) = fib(n-1) + fib(n-2)
+    add  t2, t0, t1          # fib(n)
 
-    sw   t2, 8(s0)         # guarda próximo fib na RAM
-    sw   t2, 0(s1)         # opcional: escreve saída (LED, VGA, etc)
+    andi t2, t2, 0x3FF
+    sw   t2, 8(s0)           # guarda na RAM
+    sw   t2, 0(s1)           # escreve nos LEDs
 
-    add  t0, zero, t1
-    add  t1, zero, t2
-    addi s0, s0, 4         # avança vetor
+    mv   t0, t1
+    mv   t1, t2
+    addi s0, s0, 4           # avança vetor
 
     j loop
 
